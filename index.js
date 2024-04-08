7b112de58c4f5f1aaaf27bddf24f0d4872fcaa77
Nigel.Zheng <nigel.zheng@copeland.com>
2024-04-08 10:57:13 +0800

    wait report ready

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const core = require('@actions/core')
-const newman = require('newman')
-const html = require('newman-reporter-html')
-const htmlextra = require('newman-reporter-htmlextra')
+const core = require('@actions/core');
+const newman = require('newman');
+
+const html = require('newman-reporter-html');
+html.on('ready', function() {
+  console.log('module html is ready');
+});
+const htmlextra = require('newman-reporter-htmlextra');
+htmlextra.on('ready', function() {
+  console.log('module htmlextra is ready');
+});
 
 init()
 
